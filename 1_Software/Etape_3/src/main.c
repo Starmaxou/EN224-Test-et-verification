854615a51a6c8f9d2e457e5c86050272fa90edff
Maxime <maximealberty@gmail.com>
2021-02-09 11:12:07 +0100

    Etape_3

--- 1_Software/Etape_3/src/main.c
+++ 1_Software/Etape_3/src/main.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "stdbool.h"
 #include "math.h"
 #include "time.h"
 #include "assert.h"
@@ -7,7 +8,7 @@
 #define MAX_RAND 65535
 #define MIN_RAND 0 
 
-int PGCD(int A, int B)
+int PGCD1(int A, int B)
 {
 	while(A != B){
 		if (A==0) return B;
@@ -17,6 +18,17 @@ int PGCD(int A, int B)
 		} else {
 			B = B - A;
 		}
+	}
+	return A;
+}
+
+int PGCD2(int A, int B){
+	int reste;
+
+	while(B != 0){
+		reste = A % B;
+		A = B;
+		B = reste;
 	}
 	return A;
 }
@@ -34,16 +46,20 @@ int RandB(void){
 }
 
 int main (int argc, char * argv []){
-	int A,B;
+	int A,B,pgcd_1,pgcd_2;
+	bool test = false;
 	srand(time(NULL));
 
 	printf("(II) Starting PGCD program\n");
 
-	printf("i \t A \t B \t PGCD(A,B)\n");
-	for(int i = 0 ; i < 200000 ; i++){
+	printf("i\tA\tB\tPGCD(A,B)\tVerif\n");
+	for(int i = 0 ; i < 65536 ; i++){
 		A = RandA();
 		B = RandB();
-		printf("%d\t%d\t%d\t%d\n", i, A, B,PGCD(A, B));
+		pgcd_1 = PGCD1(A,B);
+		pgcd_2 = PGCD2(A,B);
+		test = (pgcd_1==pgcd_2)?true:false;
+		printf("%d\t%d\t%d\t%d\t%d\n", i, A, B,PGCD1(A, B), test);
 	}
 	
 	printf("(II) End of PGCD program\n");
